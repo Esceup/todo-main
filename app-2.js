516cdd0a90982a9f1708e708acaf216844d24d19
Esceup <20danila.gorbunov.01@gmail.com>
2024-11-12 16:21:54 +0300

    12.11.2024 Commit

--- app-2.js
+++ app-2.js
@@ -1,22 +1,20 @@
 const btnAddItem = document.getElementById("create");
 const list = document.getElementById("list");
 const inputElement = document.getElementById("title");
+const deleteAllCompleted = document.getElementById("deleteAllCompleted");
 const deleteAll = document.getElementById("deleteAll");
 const listItem = document.getElementById("listItem"); 
-
+const allTask = document.getElementById('allTask')
+const completedTask = document.getElementById("completedTask");
 
 let notes = JSON.parse(localStorage.getItem('todosMe')) || [ ];
+
 
 
 
 const saveToLocalStorage = (key = 'todosMe') => {
   localStorage.setItem(key, JSON.stringify(notes));
 }
-
-
-
-
-
 
 // обновление списка (вызываем данную функцию каждый раз, когда изменяем или добавляем что-то в массив)
 function render() {
@@ -32,7 +30,6 @@ function render() {
   list.insertAdjacentHTML("afterbegin", getNotesTemplate(notes[i], i));
   }
   
-  
 
 //   for (let note of notes) {
 //     list.insertAdjacentHTML("afterbegin", getNotesTemplate(note));
@@ -40,8 +37,30 @@ function render() {
 }
 render();
 
+function sortNotesToCompleted() {
+ notes.sort((a, b) => (a.completed > b.completed ? 1 : -1));
+}
+sortNotesToCompleted()
 
+function countAllTask() {
+  let count = 0;
+  for (let i = 0; i < notes.length; i++) {
+    count += 1;
+  }
+  allTask.textContent = `Всего задач: ${count}`;
+}
+countAllTask();
 
+function countCompletedTask() {
+  let count = 0;
+  for (let i = 0; i < notes.length; i++) {
+    if(notes[i].completed === true) {
+      count += 1;
+    }
+  }
+  completedTask.textContent = `Выполенных задач: ${count}`;
+}
+countCompletedTask();
 
 
 btnAddItem.onclick = function () {
@@ -57,10 +76,15 @@ btnAddItem.onclick = function () {
   notes.push(newNote) // добавление в массив newNote
 
 //   list.insertAdjacentHTML("afterbegin", getNotesTemplate(newNote));
+    sortNotesToCompleted();
     saveToLocalStorage();
+    
     render(); // обновляем массив(каждый раз при изменениях)
-   
+    countAllTask();
+    countCompletedTask();
+    
     inputElement.value = "";
+
   
 };
 
@@ -75,32 +99,45 @@ list.onclick = function (event) {
         const type = event.target.dataset.type;
 
          if (type === "toggle") {  
-            notes[index].completed = !notes[index].completed    
+            notes[index].completed = !notes[index].completed      
          } else if(type === 'remove'){
            if (notes[index].completed) {
              notes.splice(index, 1);
            }
          }
+          sortNotesToCompleted();
          saveToLocalStorage();
          render();
-      
+          countAllTask();
+          countCompletedTask();
+        
     }
 }
 
 
 // удалить выполненные задачи
 
-deleteAll.onclick = function () {
+deleteAllCompleted.onclick = function () {
     notes = notes.filter((item) => item.completed !== true);
+    sortNotesToCompleted();
     saveToLocalStorage();
     render()
+    countAllTask();
+    countCompletedTask();
     
 }
 
+deleteAll.onclick = function() {
+    list.innerHTML = "";
+    sortNotesToCompleted();
+    saveToLocalStorage();
+    render();
+    countAllTask();
+    countCompletedTask();
+}
     // console.log(event.target.dataset.index)
 
 // шаблон для элемента массива с data-index и data-type
-
 function getNotesTemplate(note, index) {
   return ` 
     <li class="list-group-item bg-${
